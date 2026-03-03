3873b673eee21a523a3916b4b5e6421ff85321d6
alpaslanblc-dot <alpaslanblc@hotmail.com>
2026-03-03 17:29:01 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-const URL = "https://mvsbrknkfwwptjifdqca.supabase.co";
-const KEY = "sb_publishable_siHYnBPzrZSyHcx01nopsA_I4im8Ygr";
+// database.js
+const URL = "SENİN_URL_ADRESİN";
+const KEY = "SENİN_KEY_ADRESİN";
 
-const _db = supabase.createClient(URL, KEY);
+// window._db diyerek bunu tüm dosyaların görebileceği bir "evrensel" değişken yapıyoruz
+window._db = supabase.createClient(URL, KEY);
 
 async function getWords(params = {}) {
-    let query = _db.from('kelimeler').select('*');
-    if (params.column) query = query.eq(params.column, params.value);
+    let query = window._db.from('kelimeler').select('*');
+    if (params.column) query = query.ilike(params.column, params.value);
     if (params.search) query = query.ilike('kelime', `%${params.search}%`);
     return await query;
 }
-
