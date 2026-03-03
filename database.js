d8b4363acaefe24b6f6848eca8a5528d8fc8491f
alpaslanblc-dot <alpaslanblc@hotmail.com>
2026-03-03 17:17:35 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,22 +1,11 @@
-const SB_URL = "https://mvsbrknkfwwptjifdqca.supabase.co";
-const SB_KEY = "sb_publishable_siHYnBPzrZSyHcx01nopsA_I4im8Ygr";
-const _db = supabase.createClient(SB_URL, SB_KEY);
+const URL = "SENİN_SUPABASE_URL_ADRESİN";
+const KEY = "SENİN_SUPABASE_ANON_KEY_ADRESİN";
 
-// Veri çekme motoru
-async function getWords(options = {}) {
+const _db = supabase.createClient(URL, KEY);
+
+async function getWords(params = {}) {
     let query = _db.from('kelimeler').select('*');
-    if (options.column && options.value) {
-        if (options.op === 'ilike') query = query.ilike(options.column, options.value);
-        else query = query.eq(options.column, options.value);
-    }
-    if (options.search) query = query.or(`kelime.ilike.%${options.search}%,anlam.ilike.%${options.search}%`);
-    return await query.order('kelime');
-}
-
-async function deleteWord(id) {
-    return await _db.from('kelimeler').delete().eq('id', id);
-}
-
-async function addWord(obj) {
-    return await _db.from('kelimeler').insert([obj]);
+    if (params.column) query = query.eq(params.column, params.value);
+    if (params.search) query = query.ilike('kelime', `%${params.search}%`);
+    return await query;
 }
